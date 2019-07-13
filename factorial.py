4f17b7867a6924c3c9f062d8d2b2d7f75dbe06c0
Delo Hao <delorah0196@icloud.com>
2019-07-13 10:43:36 -0400

    update

--- factorial.py
+++ factorial.py
@@ -1,19 +1,15 @@
 
 inputnumber = int(input('Please input your number: '))
 
-numbers = [inputnumber]
+numbers = []
 if inputnumber == 0:
     print(1)
 
-while inputnumber > 1:
-    numbers.append(inputnumber - 1)
+while inputnumber:
+    numbers.append(inputnumber)
     inputnumber = inputnumber - 1
-result = 1
-# time = len(numbers) - 1
-# for number in numbers:
-#     result = numbers[time] * result
-#     time = time - 1
 
+result = 1
 for i in numbers:
     result = numbers[i - 1] * result
     print(result)
