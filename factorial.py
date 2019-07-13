92862c54cf07c66ed5b5af667e4acfa43f8f2cc1
Delo Hao <delorah0196@icloud.com>
2019-07-13 09:39:57 -0400

    update

--- factorial.py
+++ factorial.py
@@ -9,10 +9,13 @@ while inputnumber > 1:
     numbers.append(inputnumber - 1)
     inputnumber = inputnumber - 1
 result = 1
-time = len(numbers) - 1
-for number in numbers:
-    result = numbers[time] * result
-    time = time - 1
+# time = len(numbers) - 1
+# for number in numbers:
+#     result = numbers[time] * result
+#     time = time - 1
+
+for i in numbers:
+    result = numbers[i - 1] * result
     print(result)
 print('numbers length', len(numbers))
 print(result, numbers)
