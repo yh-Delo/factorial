d3eb94d776ea8279c7a122331a7005690cec0fa5
Delo Hao <delorah0196@icloud.com>
2019-07-12 20:06:28 -0400

    update

---
+++ factorial.py
@@ -0,0 +1,18 @@
+
+inputnumber = int(input('Please input your number: '))
+
+numbers = [inputnumber]
+if inputnumber == 0:
+    print(1)
+
+while inputnumber > 1:
+    numbers.append(inputnumber - 1)
+    inputnumber = inputnumber - 1
+result = 1
+time = len(numbers) - 1
+for number in numbers:
+    result = numbers[time] * result
+    time = time - 1
+    print(result)
+print('numbers length', len(numbers))
+print(result, numbers)
